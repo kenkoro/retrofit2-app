bcb98f88e16e3ed5dc0a003464d5f2fa0bd5fc94
Kirill Himura <frogharvard@gmail.com>
2023-10-22 17:07:36 +0700

    fix(renaming): rename api interface

--- app/src/main/java/com/kenkoro/retrofit2/feature_http/data/source/DummyApi.kt
+++ app/src/main/java/com/kenkoro/retrofit2/feature_http/data/source/DummyApi.kt
@@ -4,7 +4,7 @@ import com.kenkoro.retrofit2.feature_http.domain.model.Product
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-interface DummyProductApi {
+interface DummyApi {
   @GET("products/{id}")
   suspend fun getProductById(@Path("id") id: Int): Product
 }
